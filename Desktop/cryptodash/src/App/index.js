1370fad39e1d215207d58af9a2085acba37a6523
Fyardlest <fyardlest1@gmail.com>
2020-06-21 08:37:10 -0400

    AppBar Done

---
+++ Desktop/cryptodash/src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+
+function App() {
+  return (
+    <AppLayout>
+      <AppBar />
+      Hello fyardlest
+    </AppLayout>
+  );
+}
+
+export default App;
